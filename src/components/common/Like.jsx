1db8769a3715af9b9372201a496bcffaaa4d6807
Adam Kamboj <kamboj.adam@gmail.com>
2021-07-07 11:00:02 -0400

    extracted table body into component

--- src/components/common/Like.jsx
+++ src/components/common/Like.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 
-const Liked = (props) => {
+const Like = (props) => {
   const liked = props.liked ? faHeart : farHeart;
   return (
     <div style={{ cursor: "pointer" }} onClick={props.onClick}>
@@ -12,4 +12,4 @@ const Liked = (props) => {
   );
 };
 
-export default Liked;
+export default Like;
--- src/components/common/TableHeader.jsx
+++ src/components/common/TableHeader.jsx
@@ -11,6 +11,13 @@ class TableHeader extends Component {
     }
     this.props.onSort(sortColumn);
   };
+  renderSortIcon = (column) => {
+    const { sortColumn } = this.props;
+
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === "asc") return "V";
+    return "^";
+  };
 
   render() {
     return (
@@ -22,6 +29,7 @@ class TableHeader extends Component {
               onClick={() => this.raiseSort(column.path)}
             >
               {column.label}
+              {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
